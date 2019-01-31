13dbe81d3ceac76f8c3c37dbe7b5e3ae144b3d9b
Harry <harry74323@gmail.com>
2019-01-31 14:16:53 +1100

    detail page done

--- src/productList/ProductList.js
+++ src/productList/ProductList.js
@@ -13,88 +13,88 @@ class ProductList extends React.Component{
     };
   }
 
-  componentDidMount() {
-    setTimeout(()=>{
-      fetch("/api/productList.json")
-      .then(res => res.json())
-      .then(
-        (data) => {
-          this.setState({
-            isLoading: false,
-            data: {
-              productList: data
-            }
-          });
-          console.log(this.state);
-        },
-        (error) => {
-          this.setState({
-            isLoading: false,
-            error,
-          });
-        }
-      )
-    }, 1000)
-  }
+    componentDidMount() {
+        setTimeout(()=>{
+            fetch("/api/productList.json")
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    this.setState({
+                        isLoading: false,
+                        data: {
+                            productList: data
+                        }
+                    });
+                    console.log(this.state);
+                },
+                (error) => {
+                    this.setState({
+                    isLoading: false,
+                    error,
+                    });
+                }
+            )
+        }, 1000)
+    }
 
     render(){
         if (this.state.error) {
-    return <div>Error: {this.state.error.message}</div>;
+            return <div>Error: {this.state.error.message}</div>;
         } else if (this.state.isLoading) {
-    return (
-        <div className="container">
-        <h2>Loading ... </h2>
-        </div>
-    )
-        } else {
-    return (
-        <div>
-            <div className="row mb-3 mt-3">
-                <h1>Product list</h1>
-            </div>
-
-            <div className="row mb-3 form-group">    
-                <div className="col-md-4">
-                    <input className="form-control" type="text" placeholder="Search" />
+            return (
+                <div className="container">
+                <h2>Loading ... </h2>
                 </div>
-            </div>
+            )
+        } else {
+            return (
+                <div>
+                    <div className="row mb-3 mt-3">
+                        <h1>Product list</h1>
+                    </div>
 
-            <div className="row border-bottom pb-2">
-                <div className="col-md-2">Image</div>
-                <div className="col-md-2">Product name</div>
-                <div className="col-md-1">code</div>
-                <div className="col-md-1">Available</div>
-                <div className="col-md-1">Price</div>
-                <div className="col-md-2">Rating</div>
-                <div className="col-md-3"></div>
-            </div>
-
-            {
-                this.state.data.productList.map(
-                    (product)=>
-                    <div key={product.id} className="row border-bottom pb-2 pt-2">
-                        <div className="col-md-2"><img className="img-fluid" src={product.imageUrl} /></div>
-                        <div className="col-md-2">{product.productName}</div>
-                        <div className="col-md-1">{product.productCode}</div>
-                        <div className="col-md-1">{product.releaseDate}</div>
-                        <div className="col-md-1">{product.price}</div>
-                        <div className="col-md-2">{product.starRating}</div>
-                        <div className="col-md-3 text-right">
-                            <Link className="btn btn-primary btn-sm mr-1" to={`/details/${product.id}`}>View</Link>
-                            <button type="button" className="btn btn-primary btn-sm mr-1">Edit</button>
-                            <button type="button" className="btn btn-danger btn-sm">Delete</button>
+                    <div className="row mb-3 form-group">    
+                        <div className="col-md-4">
+                            <input className="form-control" type="text" placeholder="Search" />
                         </div>
                     </div>
-                )
-            }
 
-            <div className="row pt-4">
-                <div className="col-md-12 text-right">
-                    <button type="button" className="btn btn-primary">Add product</button>
+                    <div className="row border-bottom pb-2">
+                        <div className="col-md-2">Image</div>
+                        <div className="col-md-2">Product name</div>
+                        <div className="col-md-1">code</div>
+                        <div className="col-md-1">Available</div>
+                        <div className="col-md-1">Price</div>
+                        <div className="col-md-2">Rating</div>
+                        <div className="col-md-3"></div>
+                    </div>
+
+                    {
+                        this.state.data.productList.map(
+                            (product)=>
+                            <div key={product.id} className="row border-bottom pb-2 pt-2">
+                                <div className="col-md-2"><img className="img-fluid" src={product.imageUrl} /></div>
+                                <div className="col-md-2">{product.productName}</div>
+                                <div className="col-md-1">{product.productCode}</div>
+                                <div className="col-md-1">{product.releaseDate}</div>
+                                <div className="col-md-1">{product.price}</div>
+                                <div className="col-md-2">{product.starRating}</div>
+                                <div className="col-md-3 text-right">
+                                    <Link className="btn btn-primary btn-sm mr-1" to={`/details/${product.id}`}>View</Link>
+                                    <button type="button" className="btn btn-primary btn-sm mr-1">Edit</button>
+                                    <button type="button" className="btn btn-danger btn-sm">Delete</button>
+                                </div>
+                            </div>
+                        )
+                    }
+
+                    <div className="row pt-4">
+                        <div className="col-md-12 text-right">
+                            <button type="button" className="btn btn-primary">Add product</button>
+                        </div>
+                    </div>      
                 </div>
-            </div>      
-        </div>
-    );
+            );
         }
     }
 }
