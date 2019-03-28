c05357a8dddd905cf208fb9a44141b2365489090
Harry <harry74323@gmail.com>
2019-03-28 15:16:39 +1100

    build product form

--- src/productEdit/ProductEdit.js
+++ src/productEdit/ProductEdit.js
@@ -50,12 +50,97 @@ class ProductEdit extends React.Component{
             return (
                 <div>
                     <div className="row mb-3 mt-3">
-                        <h1>{this.state.data.productDetail.productName}</h1>
+                        <h1 className="col-md-12">Edit product</h1>
                     </div>
 
+                    <form className="form-horizontal">
+                        <fieldset>
+                            <div className="form-group" >
+                                <label className="control-label">Product Image</label>
+                                    <input class="form-control" 
+                                            id="imageUrl" 
+                                            type="text" 
+                                            placeholder="Image url"  />
+                            </div>
+
+                            <div className="form-group" >
+                                <label className="control-label">Product Name</label>
+                                <input className="form-control"
+                                        id='productName' 
+                                        type="text"
+                                        placeholder="Name (required)" />
+                            </div>
+
+                            <div className="form-group row">
+                                <div className="col-md-6">
+                                    <label className="control-label">Product Code</label>
+                                    <input className="form-control" 
+                                            id="productCodeId" 
+                                            type="text" 
+                                            placeholder="Code (required)" />
+                                </div>
+
+                                <div className="col-md-6">
+                                    <label className="control-label">Available</label>
+                                    <input className="form-control" 
+                                            id="dateId" 
+                                            type="date"
+                                            placeholder="Available" />
+                                </div>
+                            </div>
+
+                            <div className="form-group row">
+                                <div className="col-md-6">
+                                    <label className="control-label">Price</label>
+                                    <input className="form-control"
+                                            id="priceId" 
+                                            type="number"
+                                            placeholder="Price" />
+                                </div>
+
+                                <div className="col-md-6">
+                                    <label className="control-label">Rating (1-5)</label>
+                                    <input className="form-control"
+                                            id="starRatingId" 
+                                            type="number"
+                                            min="1" 
+                                            max="5"
+                                            step="1" 
+                                            placeholder="Rating" />
+                                </div>
+                            </div>
+                        
+                            <div className="form-group">
+                                <label className="control-label">Description</label>
+                                <textarea class="form-control" 
+                                        id="descriptionId" 
+                                        placeholder="Description"
+                                        rows="3"></textarea>
+                            </div>
+
+                            <div>
+                                <label className="control-label">Tags</label>
+                                <div className="form-group row">
+                                    <div className="col-md-3">
+                                        <input class="form-control"
+                                                type="text"
+                                                placeholder="Tag" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="text-right">
+                                <button className="btn btn-primary btn-sm"
+                                        type="button">Add Tag
+                                </button>
+                            </div>
+                        </fieldset>
+                    </form>
+                    <hr />
                     <div className="row pt-4">
-                        <div className="col-md-12 text-right">
-                            <Link className="btn btn-primary btn-sm mr-1" to={`/`}>Back to list</Link>
+                        <div class="col-md-12 text-right">
+                            <Link className="btn btn-danger mr-3" to={`/`}>Cancel</Link>
+                            <button type="button" className="btn btn-primary">Save</button>
                         </div>
                     </div>
                 </div>
